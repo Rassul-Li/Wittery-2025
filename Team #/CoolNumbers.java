703e0def11bef162c69a74eb492ec7907225d95e
Mia-Dzinic <mia.dzinic25@gmail.com>
2025-03-01 11:46:19 -0800

    Update CoolNumbers.java

--- Team #/CoolNumbers.java
+++ Team #/CoolNumbers.java
@@ -14,53 +14,85 @@ public class CoolNumbers
      */
     public static int getDensce(int num)
     {
-        if (num == 7) return 1;
-        if (num == 316) return 2;
-        if (num == 886) return 1;
-        if (num == 14319) return 4;
-        if (num == 27867) return 0;
-        if (num == 16) return 0;
+        String s = Integer.toBinaryString(num);
+        ArrayList<Integer> arr = new ArrayList<>();
+        int count = 0;
+        for(int x=0; x<s.length(); x++){
+            if(s.charAt(x)=='1'){
+                count++;
+            }else {
+                arr.add(count);
+                count = 0;
+            }
+        }
+        arr.add(count);
+        int max = arr.get(0);
+        for(int x=0; x<arr.size(); x++){
+            if(arr.get(x)>max){
+                max = arr.get(x);
+            }
+        }
+        if(max<2){
+            return 0;
+        }
+        return max-2;
 
-        return -1;
     }
 
     /*
-     * Shavar rating: every occurrence of a ‘101’ increases the score by 2,
+     * Shavar rating: every occurrence of a ï¿½101ï¿½ increases the score by 2,
      * unless a 1 is being shared, then the score is increased by 1.
      * In the example below, 21 has a score of 3. (The middle digit is used in both occurrences of 101.)
      * The binary String 101101 (45 =  32+8+4 + 1) has a score of 4
      */
     public static int getShavar(int num)
     {
-        if (num == 21) return 3;
-        if (num == 45) return 4;
-        if (num == 429) return 5;
-        if (num == 5461) return 7;
-        if (num == 85) return 4;
-        if (num == 725) return 6;
+        String s = Integer.toBinaryString(num);
 
-        return -1;
-    }  
+        return recursive(s);
+    }
+    public static int recursive(String s){
+
+        if(s.length()<2){
+            return 0;
+        }else if(s.length()>2&&s.charAt(0)=='1'&& s.charAt(1)=='0'&&s.charAt(2)=='1'){
+            if(s.length()>3){
+                if(s.charAt(3)=='0'){
+                    if(s.length()>4&&s.charAt(4)=='1'){
+                        return 1 + recursive(s.substring(2,s.length()));
+                    }else{
+                        return 2 + recursive(s.substring(3,s.length()));
+                    }
+                }else{
+                    return 2 + recursive(s.substring(3,s.length()));
+                }
+            }else{
+                return 2;
+            }
+
+
+
+        }else{
+            return recursive(s.substring(1,s.length()));
+        }
+
+    }
 
     /*
      * Coolness is the  sum of Densce score and Shavar rating
      */
     public static int getCoolness(int num)
     {
-        if (num == 117) return 4;
-        if (num == 99) return 0;
-        if (num == 6101) return 9;
-        if (num == 14319) return 8;
-
-        return -1;
+        return getDensce(num) + getShavar(num);
     }
 
     public static int getNextNumberWithGreaterCoolness(int num)
     {
-        if (num == 99) return 101;
-        if (num == 101) return 106;
-        if (num == 253) return 381;
-
-        return -1;
+        int n = getDensce(num) + getShavar(num);
+        int count = n+1;
+        while(getDensce(count)+getShavar(count)<n){
+            count++;
+        }
+        return count;
     }
 }
